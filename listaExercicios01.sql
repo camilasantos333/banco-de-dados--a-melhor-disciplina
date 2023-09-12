fa33654571da9ac9a1de39fa5da0c81613b95392
camilasantos333 <143538290+camilasantos333@users.noreply.github.com>
2023-09-11 22:43:20 -0300

    3

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -1,3 +1,8 @@
 SELECT titulo FROM livros;
 
 SELECT nome, nascimento FROM autores WHERE YEAR(nascimento) < 1900;
+
+SELECT l.titulo
+FROM livros l
+JOIN autores a ON l.autor_id = a.id
+WHERE a.nome = 'J.K. Rowling';
