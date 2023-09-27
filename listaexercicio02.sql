e198648d93afe96733cf888357a47699e18a8128
camilasantos333 <143538290+camilasantos333@users.noreply.github.com>
2023-09-26 22:53:48 -0300

    6

--- listaexercicio02.sql
+++ listaexercicio02.sql
@@ -55,3 +55,24 @@ BEGIN
 END //
 DELIMITER ;
 CALL sp_LivrosAteAno(2012);
+DELIMITER //
+CREATE PROCEDURE sp_TitulosPorCategoria(IN categoria_nome VARCHAR(100))
+BEGIN
+    DECLARE done INT DEFAULT 0;
+    DECLARE livro_titulo VARCHAR(255);
+    DECLARE cur CURSOR FOR
+        SELECT Livro.Titulo
+        FROM Livro
+        INNER JOIN Categoria ON Livro.Categoria_ID = Categoria.Categoria_ID
+        WHERE Categoria.Nome = categoria_nome;
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
+    OPEN cur;
+    FETCH cur INTO livro_titulo;
+    WHILE NOT done DO
+        SELECT livro_titulo AS 'Título do Livro';
+        FETCH cur INTO livro_titulo;
+    END WHILE;
+    CLOSE cur;
+END //
+DELIMITER ;
+CALL sp_TitulosPorCategoria('Ficção Científica');
